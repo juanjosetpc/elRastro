173564b61922113766c8ae243f15c9797b583708
juanjose <juanjosetp02@uma.es>
2023-10-26 00:21:13 +0200

    crud de productos funcionando+ 2 querys simples sobre producto + postman para lo implementado de productos

--- Backend/controllers/producto.js
+++ Backend/controllers/producto.js
@@ -47,26 +47,19 @@ const createProduct = async (req, res) => {
 };
 
 const updateProduct = async (req, res) => {
-  const email = req.params.email;
-  const titulo = req.params.titulo;
+  const id = req.params.id;
   const datosActualizar = req.body;
 
   try {
-    const producto = await Producto.find({
-      titulo: titulo,
-      emailVendedor: email,
+    const producto = await Producto.findByIdAndUpdate(id, datosActualizar, {
+      new: true,
     });
 
     if (!producto) {
       return res.status(404).json({ error: "Producto no encontrado" });
     }
 
-    const nuevoProducto = await Producto.findByIdAndUpdate(
-      producto._id,
-      datosActualizar
-    );
-
-    res.status(200).json({ mensaje: "Producto actualizado", nuevoProducto });
+    res.status(200).json({ mensaje: "Producto actualizado", producto });
   } catch (error) {
     res.status(500).json({
       error: "Error al actualizar el producto. Error msg: " + error.message,
@@ -75,34 +68,36 @@ const updateProduct = async (req, res) => {
 };
 
 const deleteProduct = async (req, res) => {
-  const email = req.params.email;
-  const titulo = req.params.titulo;
+  const id = req.params.id;
   try {
-    const producto = await Producto.findAndDelete({
-      titulo: titulo,
-      emailVendedor: email,
-    });
+    const producto = await Producto.findByIdAndDelete(id);
 
     if (!producto) {
       return res.status(404).json({ error: "Producto no encontrado" });
     }
-    res.status(204).send(); // Respuesta exitosa sin contenido
+    res.status(204); // Respuesta exitosa sin contenido
   } catch (error) {
     res.status(500).json({ error: "Error al intentar borrar el producto" });
   }
 };
+
 //Obtener productos ofertados por un usuario descendente por fecha de inicio
 const getProductsByUserDescByDate = async (req, res) => {
   const vendedor = req.params.email;
-
-  const productos = await Producto.find({ emailVendedor: vendedor });
-  productos.sort({ fechaInicio: "desc" });
-
-  res.json(productos);
+  try {
+    const productos = await Producto.find({ emailVendedor: vendedor }).sort({
+      fechaInicio: "desc",
+    });
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({
+      error:
+        "Error al obtener los productos. Mensaje de error: " + error.message,
+    });
+  }
 };
 
-//Producto en subasta a partir de (parte de ) su descripción
-
+//Producto en subasta a partir de (parte de) su descripción
 const getProductsByDescription = async (req, res) => {
   const descripcion = req.params.descripcion;
 
@@ -110,12 +105,16 @@ const getProductsByDescription = async (req, res) => {
     // Realiza una consulta para encontrar productos en subasta que contengan la descripción parcial
     const productos = await Producto.find({
       enSubasta: true, // Solo productos en subasta
-      descripcion: { $regex: descripcionParcial, $options: "i" }, // Búsqueda con expresión regular, sin distinción entre mayúsculas y minúsculas
+      descripcion: { $regex: descripcion, $options: "i" }, // Búsqueda con expresión regular, sin distinción entre mayúsculas y minúsculas
     });
 
     res.status(200).json(productos);
   } catch (error) {
-    res.status(500).json({ error: "Error al buscar productos en subasta" });
+    res.status(500).json({
+      error:
+        "Error al buscar productos en subasta. Mensaje de error: " +
+        error.message,
+    });
   }
 };
 
