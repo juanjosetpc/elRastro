2a63c8e775374b4cf201f672829b77f98a5039b4
Antonio <antonionavajaortega@uma.es>
2023-10-25 11:22:42 +0200

    OpenStreetMap con MapTiler

--- Frontend/client/src/mapa/mapa.js
+++ Frontend/client/src/mapa/mapa.js
@@ -1,33 +1,72 @@
-import React from 'react';
-//import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
 import  buscarDireccion  from './MapaApi';
+import React, {useState, useEffect } from 'react';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import { XYZ } from 'ol/source';
+import axios from 'axios';
+import { useGeographic } from 'ol/proj';
 
-import {
-    interaction, layer, custom, control, //name spaces
-    Interactions, Overlays, Controls,     //group
-    Map, Layers, Overlay, Util    //objects
-  } from "react-openlayers";
 
 const direccion = "Vialia Centro, Málaga"
 const coordenadas = await buscarDireccion(direccion)
 
-const MapaComponent = () => {
- 
-  return (
-    <div>
-      <h1>Mapa</h1>
-      
-      <Map view={{center: [coordenadas.lon,  coordenadas.lat], zoom: 1}} >
-      <Layers>
-        <layer.Tile></layer.Tile>
-      </Layers>
-     
-    </Map>
+const longitude = coordenadas.lon
+const latitude = coordenadas.lat
 
-  
-    </div>
-  );
+export function useGeographicCoordinates(longitude, latitude) {
+  useGeographic(); // Call useGeographic inside the hook
+
+  const [coordinates, setCoordinates] = useState([longitude, latitude]);
+
+  useEffect(() => {
+    setCoordinates([longitude, latitude]);
+  }, [longitude, latitude]);
+
+  return coordinates;
+}
+
+const MapComponent = () => {
+
+  const geographicCoordinates = useGeographicCoordinates(longitude, latitude);
+
+  useEffect(() => {
+    // Obtén el token de acceso de MapTiler
+    axios
+      .get('https://api.maptiler.com/maps/basic/tiles.json?key=j6HKPeHb75jzLbrTMEGy')
+      .then((response) => {
+        const { tiles } = response.data;
+
+        // Crea una nueva capa de mosaico utilizando la fuente de MapTiler
+        const tileLayer = new TileLayer({
+          source: new XYZ({
+            urls: tiles,
+            crossOrigin: 'anonymous',
+          }),
+        });
+
+        // Crea el mapa con la capa de mosaico de MapTiler
+        const map = new Map({
+          target: 'map',
+          layers: [tileLayer],
+          view: new View({
+            center: geographicCoordinates, // Centro del mapa [longitud, latitud]
+            zoom: 16, // Nivel de zoom inicial
+          }),
+        });
+
+        return () => {
+          // Limpieza cuando el componente se desmonta
+          map.setTarget(null);
+        };
+      })
+      .catch((error) => {
+        console.error('Error al obtener los tiles de MapTiler:', error);
+      });
+  }, [geographicCoordinates]); // El arreglo vacío asegura que este efecto se ejecute solo una vez después del montaje inicial
+
+  return <div id="map" style={{ width: '100%', height: '100vh' }}></div>;
 };
 
-export default MapaComponent;
+export default MapComponent;
