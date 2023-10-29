bdf3b76cde019f91d8f71688d5caf652c3d198cd
Antonio <antonionavajasortega@uma.es>
2023-10-29 18:32:57 +0100

    CRUD básico de puja terminado, más las peticiones en postman

--- Backend/controllers/puja.js
+++ Backend/controllers/puja.js
@@ -2,20 +2,87 @@ const Puja = require("../models/puja");
 
 const getAllPujas = async (req, res) => {
   try {
-    const pujas = await Puja.find();
-    res.json(pujas);
+    const pujasS = await Puja.find();
+    if (!pujasS || pujasS.length === 0) {
+      return res.status(404).json({ error: "No se encontraron pujas" });
+    }
+    res.json(pujasS);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: "Error al obtener los pujas" });
   }
 };
 
-async function createPuja(req, res) {
+const createPuja = async (req, res) => {
   try {
-    const nuevaPuja = await Puja.createPuja(req.body);
-    res.json(nuevaPuja);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-}
+    const pujas = req.body;
+    const nuevasPujas = [];
 
-module.exports = { getAllPujas, createPuja };
+    for (const puja of pujas) {
+      // Comprobar si ya existe una puja con los mismos datos
+      const pujaExistente = await Puja.findOne({
+        producto: puja.producto,
+        cantidad: puja.cantidad,
+        emailPujador: puja.emailPujador
+      });
+
+      if (pujaExistente) {
+        // Si la puja ya existe, puedes omitirla o manejarla según tus necesidades
+        console.log(`La puja ya existe: ${pujaExistente.titulo}`);
+      } else {
+        // Si la puja no existe, créala y agrégala a la lista de nuevas pujas
+        const nuevaPuja = await Puja.create(puja);
+        nuevasPujas.push(nuevaPuja);
+      }
+    }
+
+    if (nuevasPujas.length == 0) {
+      res.status(201).send("Las pujas ya estaban en la base de datos");
+    } else {
+      res.status(201).json({ nuevasPujas });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al crear las pujas" });
+  }
+};
+
+const updatePuja = async (req, res) => {
+  const id = req.params.id;
+  const datosActualizar = req.body;
+
+  try {
+    const puja = await Puja.findByIdAndUpdate(id, datosActualizar, {
+      new: true,
+    });
+
+    if (!puja) {
+      return res.status(404).json({ error: "Puja no encontrado" });
+    }
+
+    res.status(200).json({ mensaje: "Puja actualizado", puja });
+  } catch (error) {
+    res.status(500).json({
+      error: "Error al actualizar la puja. Error msg: " + error.message,
+    });
+  }
+};
+
+const deletePuja = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const puja = await Puja.findByIdAndDelete(id);
+
+    if (!puja) {
+      return res.status(404).json({ error: "Puja no encontrado" });
+    }
+    res.status(200).json({ mensaje: "Puja eliminada correctamente" }); // Respuesta exitosa sin contenido
+  } catch (error) {
+    res.status(500).json({ error: "Error al intentar borrar la puja" });
+  }
+};
+
+
+
+
+
+
+module.exports = { getAllPujas, createPuja, updatePuja, deletePuja };
--- Backend/v1/routes/pujaRoutes.js
+++ Backend/v1/routes/pujaRoutes.js
@@ -5,5 +5,8 @@ const pujaController = require("../../controllers/puja");
 
 router.get("/", pujaController.getAllPujas); // listar pujas
 router.post("/", pujaController.createPuja); // crear nueva puja
+router.put("/:id", pujaController.updatePuja); // actualizar producto
+router.delete("/:id", pujaController.deletePuja); // eliminar producto
+
 
 module.exports = router;
