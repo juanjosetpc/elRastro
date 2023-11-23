9a364c55fa0c6a622156b8a9e14cd92143073f1d
DESKTOP-TAADH65\josem <josemanuelsannav@uma.es>
2023-11-23 19:57:21 +0100

    modified:   Frontend/client/src/components/BotonPujar.jsx 	modified:   Frontend/client/src/pages/ProductDetail.jsx

--- Frontend/client/src/components/BotonPujar.jsx
+++ Frontend/client/src/components/BotonPujar.jsx
@@ -1,24 +1,98 @@
+ import React, { useState } from 'react';
+ import axios from 'axios';
+ import Popup from 'reactjs-popup';
+import '../styles/BotonPujar.css'; // Importa el archivo CSS
+
+ const BotonPujar = ({producto,emailPujador,onPujaRealizada}) => {
+   const [precio, setPrecio] = useState(''); //precio es la variable donde se guarda la puja
+const [mostrarPopup, setMostrarPopup] = useState(false);
+
+
+const handlePujar = async () => {
+  if (precio > producto.pujaMayor) {
+    console.log("Email pujador: " + emailPujador);
+    setMostrarPopup(true);
+
+    // Ocultar el popup después de 2 segundos (ajusta según tus necesidades)
+    setTimeout(() => {
+      setMostrarPopup(false);
+    }, 2000);
+
+    // Realizar la llamada a la API
+    const response = await axios.post(`http://localhost:5000/api/v1/pujas/`, {
+      producto: producto._id,
+      cantidad: precio,
+      emailPujador: emailPujador
+    });
+
+    // Llamar a la función proporcionada para indicar que la puja ha sido realizada
+    onPujaRealizada();
+  } else {
+    console.log("ERROR: la puja es menor");
+  }
+};
+
+return (
+  <div style={{ textAlign: 'center', maxWidth: '400px', margin: 'auto', padding: '20px' }}>
+    <h2>Pujar</h2>
+    <input
+      type="number"
+      placeholder="Ingrese el precio"
+      value={precio}
+      onChange={(e) => setPrecio(e.target.value)}
+      style={{ padding: '10px', marginRight: '10px' }}
+    />
+    <button onClick={handlePujar} style={{ padding: '10px' }}>
+      Pujar
+    </button>
+
+    {/* Mostrar el popup si mostrarPopup es verdadero */}
+    {mostrarPopup && (
+      <div className="popup">
+        <p>Puja Realizada</p>
+      </div>
+    )}
+  </div>
+);
+};
+
+
+
+ export default BotonPujar;
+
+
+// // BotonPujar.jsx
 // import React, { useState } from 'react';
 // import axios from 'axios';
-// import Popup from 'reactjs-popup';
+// import '../styles/BotonPujar.css'; // Importa el archivo CSS
 
-// const BotonPujar = ({producto,emailPujador}) => {
-//   const [precio, setPrecio] = useState(''); //precio es la variable donde se guarda la puja
-  
+// const BotonPujar = ({ producto, emailPujador, onPujaRealizada }) => {
+//   const [precio, setPrecio] = useState('');
+//   const [mostrarPopup, setMostrarPopup] = useState(false);
 
-//   //Lo que ocurre al pulsar el boton
-//   const handlePujar = async () => {//Si se ejeculta, para ver el consolelog, en google : inspeccionar y entrar en la pestaña consola
-//         if(precio > producto.pujaMayor){
-//           console.log("Email pujador : "+emailPujador );
-//           const response = await axios.post(`http://localhost:5000/api/v1/pujas/`, { "producto": producto._id,"cantidad": precio,"emailPujador": emailPujador });
+//   const handlePujar = async () => {
+//     if (precio > producto.pujaMayor) {
+//       console.log("Email pujador: " + emailPujador);
+//       setMostrarPopup(true);
 
-          
+//       // Ocultar el popup después de 2 segundos (ajusta según tus necesidades)
+//       setTimeout(() => {
+//         setMostrarPopup(false);
+//       }, 2000);
 
-//         }else{
-//             console.log("ERROR : la puja es menor");
-//         }
-    
-//     };
+//       // Realizar la llamada a la API
+//       const response = await axios.post(`http://localhost:5000/api/v1/pujas/`, {
+//         producto: producto._id,
+//         cantidad: precio,
+//         emailPujador: emailPujador
+//       });
+
+//       // Llamar a la función proporcionada para indicar que la puja ha sido realizada
+//       onPujaRealizada();
+//     } else {
+//       console.log("ERROR: la puja es menor");
+//     }
+//   };
 
 //   return (
 //     <div style={{ textAlign: 'center', maxWidth: '400px', margin: 'auto', padding: '20px' }}>
@@ -31,73 +105,17 @@
 //         style={{ padding: '10px', marginRight: '10px' }}
 //       />
 //       <button onClick={handlePujar} style={{ padding: '10px' }}>
-//          Pujar
+//         Pujar
 //       </button>
-      
+
+//       {/* Mostrar el popup si mostrarPopup es verdadero */}
+//       {mostrarPopup && (
+//         <div className="popup">
+//           <p>Puja Realizada</p>
+//         </div>
+//       )}
 //     </div>
 //   );
 // };
 
-
-
 // export default BotonPujar;
-
-
-// BotonPujar.jsx
-import React, { useState } from 'react';
-import axios from 'axios';
-import '../styles/BotonPujar.css'; // Importa el archivo CSS
-
-const BotonPujar = ({ producto, emailPujador, onPujaRealizada }) => {
-  const [precio, setPrecio] = useState('');
-  const [mostrarPopup, setMostrarPopup] = useState(false);
-
-  const handlePujar = async () => {
-    if (precio > producto.pujaMayor) {
-      console.log("Email pujador: " + emailPujador);
-      setMostrarPopup(true);
-
-      // Ocultar el popup después de 2 segundos (ajusta según tus necesidades)
-      setTimeout(() => {
-        setMostrarPopup(false);
-      }, 2000);
-
-      // Realizar la llamada a la API
-      const response = await axios.post(`http://localhost:5000/api/v1/pujas/`, {
-        producto: producto._id,
-        cantidad: precio,
-        emailPujador: emailPujador
-      });
-
-      // Llamar a la función proporcionada para indicar que la puja ha sido realizada
-      onPujaRealizada();
-    } else {
-      console.log("ERROR: la puja es menor");
-    }
-  };
-
-  return (
-    <div style={{ textAlign: 'center', maxWidth: '400px', margin: 'auto', padding: '20px' }}>
-      <h2>Pujar</h2>
-      <input
-        type="number"
-        placeholder="Ingrese el precio"
-        value={precio}
-        onChange={(e) => setPrecio(e.target.value)}
-        style={{ padding: '10px', marginRight: '10px' }}
-      />
-      <button onClick={handlePujar} style={{ padding: '10px' }}>
-        Pujar
-      </button>
-
-      {/* Mostrar el popup si mostrarPopup es verdadero */}
-      {mostrarPopup && (
-        <div className="popup">
-          <p>Puja Realizada</p>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default BotonPujar;
--- Frontend/client/src/pages/ProductDetail.jsx
+++ Frontend/client/src/pages/ProductDetail.jsx
@@ -1,96 +1,12 @@
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import api from '../services/api';
-// import { Carousel } from 'react-responsive-carousel'; // Asegúrate de tener esta línea de importación
-// import 'react-responsive-carousel/lib/styles/carousel.min.css';
-// import BotonPujar from '../components/BotonPujar.jsx';
-// import Mapa from '../components/Mapa.jsx';
-//  import axios from 'axios';
-
-
-// const ProductDetail = ({ propEmail }) => {
-//   const { id } = useParams();
-//   const [producto, setProducto] = useState(null);
-
-//   useEffect(() => {
-//     const fetchProductDetails = async () => {
-//       try {
-//         const response = await api.get(`/productos/${id}`);
-//         setProducto(response.data);
-        
-
-//       } catch (error) {
-//         console.error('Error fetching product details:', error);
-//       }
-//     };
-
-//     fetchProductDetails();
-//   }, [id,producto]);
-//   //Al poner el id como dependencia, nos aseguramos que se ejecute este useEffect cada vez
-//   //que cambie el id, es decir aseguramos que cada vez que cambiamos de producto
-//   //se hace la búsqueda de este con su id correspondiente.
-
-//   return (
-//     <>
-//       {producto && (
-//         <div style={{ textAlign: 'center', maxWidth: '600px', margin: 'auto', padding: '20px' }}>
-//           {/* Renderiza los detalles del producto aquí utilizando el estado producto */}
-//           <p>ID: {producto._id}</p>
-//           <h1>{producto.titulo}</h1>
-//           {/* Agrega más detalles según la estructura de tu objeto producto */}
-//           {/* Contenedor del Carrusel */}
-//           <div style={{ maxWidth: '300px', margin: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-//               {/* Carrusel de imágenes */}
-//               {producto.fotos && producto.fotos.length > 0 && (
-//                 <Carousel>
-//                   {producto.fotos.map((imagen, index) => (
-//                     <div key={index} style={{ maxWidth: '300px', maxHeight: '200px', margin: 'auto' }}>
-//                       <img
-//                         src={imagen}
-//                         alt={`Imagen ${index + 1}`}
-//                         style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-//                       />
-//                     </div>
-//                   ))}
-//                 </Carousel>
-//               )}
-//           </div>
-//           <div>
-//             <h2>Localizacion</h2>
-//             <p>{producto.direccion}</p>
-//             <div className='Mapa'>
-//                 <Mapa direccion = {producto.direccion}/>
-//             </div>
-//           </div>
-//           <div>
-//             <h2>Pujas</h2>
-//             <p>Precio de inicio: {producto.precioInicio}</p>
-//             <p>Precio actual de la puja : {producto.pujaMayor}</p>
-//             {producto.emailVendedor === propEmail ?( <p></p>) : (
-//                 <div className="botonPuja">
-//                     <BotonPujar  producto = {producto} emailPujador = { propEmail }/>
-//                  </div>
-//             ) }
-            
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default ProductDetail;
-
-
-// ProductDetail.jsx
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../services/api';
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from 'react-responsive-carousel'; // Asegúrate de tener esta línea de importación
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import BotonPujar from '../components/BotonPujar.jsx';
 import Mapa from '../components/Mapa.jsx';
-import axios from 'axios';
+ import axios from 'axios';
+
 
 const ProductDetail = ({ propEmail }) => {
   const { id } = useParams();
@@ -102,35 +18,64 @@ const ProductDetail = ({ propEmail }) => {
       try {
         const response = await api.get(`/productos/${id}`);
         setProducto(response.data);
+        
+
       } catch (error) {
         console.error('Error fetching product details:', error);
       }
     };
 
     fetchProductDetails();
-  }, [id, producto, pujaRealizada]);
-
+  }, [id,producto,pujaRealizada]);
+  //Al poner el id como dependencia, nos aseguramos que se ejecute este useEffect cada vez
+  //que cambie el id, es decir aseguramos que cada vez que cambiamos de producto
+  //se hace la búsqueda de este con su id correspondiente.
   const handlePujaRealizada = () => {
-    // Puedes realizar acciones adicionales aquí si es necesario
-    setPujaRealizada(true);
-  };
-
+      // Puedes realizar acciones adicionales aquí si es necesario
+       setPujaRealizada(true);
+       };
   return (
     <>
       {producto && (
         <div style={{ textAlign: 'center', maxWidth: '600px', margin: 'auto', padding: '20px' }}>
+          {/* Renderiza los detalles del producto aquí utilizando el estado producto */}
           <p>ID: {producto._id}</p>
           <h1>{producto.titulo}</h1>
-          <h1>{producto.pujaMayor}</h1>
-          {/* Resto de tu código para mostrar detalles de producto */}
-
-          {producto.emailVendedor === propEmail ? (
-            <p></p>
-          ) : (
-            <div className="botonPuja">
-              <BotonPujar producto={producto} emailPujador={propEmail} onPujaRealizada={handlePujaRealizada} />
+          {/* Agrega más detalles según la estructura de tu objeto producto */}
+          {/* Contenedor del Carrusel */}
+          <div style={{ maxWidth: '300px', margin: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+              {/* Carrusel de imágenes */}
+              {producto.fotos && producto.fotos.length > 0 && (
+                <Carousel>
+                  {producto.fotos.map((imagen, index) => (
+                    <div key={index} style={{ maxWidth: '300px', maxHeight: '200px', margin: 'auto' }}>
+                      <img
+                        src={imagen}
+                        alt={`Imagen ${index + 1}`}
+                        style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+                      />
+                    </div>
+                  ))}
+                </Carousel>
+              )}
+          </div>
+          <div>
+            <h2>Localizacion</h2>
+            <p>{producto.direccion}</p>
+            <div className='Mapa'>
+                <Mapa direccion = {producto.direccion}/>
             </div>
-          )}
+          </div>
+          <div>
+            <h2>Pujas</h2>
+            <p>Precio de inicio: {producto.precioInicio}</p>
+            <p>Precio actual de la puja : {producto.pujaMayor}</p>
+            {producto.emailVendedor === propEmail ?( <p></p>) : (
+                <div className="botonPuja">
+                <BotonPujar producto={producto} emailPujador={propEmail} onPujaRealizada={handlePujaRealizada} />                 </div>
+            ) }
+            
+          </div>
         </div>
       )}
     </>
@@ -138,3 +83,61 @@ const ProductDetail = ({ propEmail }) => {
 };
 
 export default ProductDetail;
+
+
+// // ProductDetail.jsx
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import api from '../services/api';
+// import { Carousel } from 'react-responsive-carousel';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import BotonPujar from '../components/BotonPujar.jsx';
+// import Mapa from '../components/Mapa.jsx';
+// import axios from 'axios';
+
+// const ProductDetail = ({ propEmail }) => {
+//   const { id } = useParams();
+//   const [producto, setProducto] = useState(null);
+//   const [pujaRealizada, setPujaRealizada] = useState(false);
+
+//   useEffect(() => {
+//     const fetchProductDetails = async () => {
+//       try {
+//         const response = await api.get(`/productos/${id}`);
+//         setProducto(response.data);
+//       } catch (error) {
+//         console.error('Error fetching product details:', error);
+//       }
+//     };
+
+//     fetchProductDetails();
+//   }, [id, producto, pujaRealizada]);
+
+//   const handlePujaRealizada = () => {
+//     // Puedes realizar acciones adicionales aquí si es necesario
+//     setPujaRealizada(true);
+//   };
+
+//   return (
+//     <>
+//       {producto && (
+//         <div style={{ textAlign: 'center', maxWidth: '600px', margin: 'auto', padding: '20px' }}>
+//           <p>ID: {producto._id}</p>
+//           <h1>{producto.titulo}</h1>
+//           <h1>{producto.pujaMayor}</h1>
+//           {/* Resto de tu código para mostrar detalles de producto */}
+
+//           {producto.emailVendedor === propEmail ? (
+//             <p></p>
+//           ) : (
+//             <div className="botonPuja">
+//               <BotonPujar producto={producto} emailPujador={propEmail} onPujaRealizada={handlePujaRealizada} />
+//             </div>
+//           )}
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default ProductDetail;
