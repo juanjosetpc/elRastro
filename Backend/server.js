27b4ccbf17ae5d2e141733542e3f99b5f0a457eb
Antonio <antonionavajasortega@uma.es>
2023-11-25 18:17:32 +0100

    cosas del env que me va mal y se me olvida dejarlo como estaba

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const v2UsuarioRouter = require("./v2/routes/usuarioRoutes2");
 const {closeConversation} = require("./controllers/conversacion");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
@@ -40,7 +40,7 @@ app.listen(PORT, () => {
 // });
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://juanjosetp02:rootrastro@clusterrastro.fbgytqb.mongodb.net/elrastro?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGO_URI);
 
 // Control de errores
 const db = mongoose.connection;
