c26436a52a8865d8702236cc09f8d5cf46da8278
DESKTOP-TAADH65\josem <josemanuelsannav@uma.es>
2023-11-25 14:10:22 +0100

    modified:   Frontend/client/src/components/BotonPujar.jsx 	modified:   Frontend/client/src/pages/ProductDetail.jsx

--- Frontend/client/src/components/BotonPujar.jsx
+++ Frontend/client/src/components/BotonPujar.jsx
@@ -6,11 +6,10 @@ import '../styles/BotonPujar.css'; // Importa el archivo CSS
 const BotonPujar = ({ producto, emailPujador, onPujaRealizada }) => {
   const [precio, setPrecio] = useState(''); //precio es la variable donde se guarda la puja
   const [mostrarPopup, setMostrarPopup] = useState(false);
-
+  const [mostrarPopupError, setMostrarPopupError] = useState(false);
 
   const handlePujar = async () => {
     if (precio > producto.pujaMayor && precio > producto.precioInicio) {
-      console.log("Email pujador: " + emailPujador);
       setMostrarPopup(true);
 
       // Ocultar el popup después de 2 segundos (ajusta según tus necesidades)
@@ -28,13 +27,18 @@ const BotonPujar = ({ producto, emailPujador, onPujaRealizada }) => {
       // Llamar a la función proporcionada para indicar que la puja ha sido realizada
       onPujaRealizada();
     } else {
-      console.log("ERROR: la puja es menor");
+      setMostrarPopupError(true);
+      // Ocultar el popup después de 2 segundos (ajusta según tus necesidades)
+      setTimeout(() => {
+        setMostrarPopupError(false);
+      }, 3000);
+      
     }
   };
 
   return (
     <div>
-    {mostrarPopup && <div className="overlay"></div>}
+    {(mostrarPopup && <div className="overlay"></div>) || (mostrarPopupError && <div className="overlay"></div>) }
     <div style={{ textAlign: 'center', maxWidth: '400px', margin: 'auto', padding: '20px' }}>
       <h2>Pujar</h2>
       <input
@@ -55,6 +59,13 @@ const BotonPujar = ({ producto, emailPujador, onPujaRealizada }) => {
           <p>Puja Realizada</p>
         </div>
       )  }
+      {/* Mostrar el popupError si mostrarPopup es verdadero */}
+      {mostrarPopupError && (
+        
+        <div className="popup">
+          <p>La puja tiene que ser mayor al precio de inicio y al de la puja mayor</p>
+        </div>
+      )  }
     </div>
     </div>
   );
@@ -64,62 +75,3 @@ const BotonPujar = ({ producto, emailPujador, onPujaRealizada }) => {
 
 export default BotonPujar;
 
-
-// // BotonPujar.jsx
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import '../styles/BotonPujar.css'; // Importa el archivo CSS
-
-// const BotonPujar = ({ producto, emailPujador, onPujaRealizada }) => {
-//   const [precio, setPrecio] = useState('');
-//   const [mostrarPopup, setMostrarPopup] = useState(false);
-
-//   const handlePujar = async () => {
-//     if (precio > producto.pujaMayor) {
-//       console.log("Email pujador: " + emailPujador);
-//       setMostrarPopup(true);
-
-//       // Ocultar el popup después de 2 segundos (ajusta según tus necesidades)
-//       setTimeout(() => {
-//         setMostrarPopup(false);
-//       }, 2000);
-
-//       // Realizar la llamada a la API
-//       const response = await axios.post(`http://localhost:5000/api/v1/pujas/`, {
-//         producto: producto._id,
-//         cantidad: precio,
-//         emailPujador: emailPujador
-//       });
-
-//       // Llamar a la función proporcionada para indicar que la puja ha sido realizada
-//       onPujaRealizada();
-//     } else {
-//       console.log("ERROR: la puja es menor");
-//     }
-//   };
-
-//   return (
-//     <div style={{ textAlign: 'center', maxWidth: '400px', margin: 'auto', padding: '20px' }}>
-//       <h2>Pujar</h2>
-//       <input
-//         type="number"
-//         placeholder="Ingrese el precio"
-//         value={precio}
-//         onChange={(e) => setPrecio(e.target.value)}
-//         style={{ padding: '10px', marginRight: '10px' }}
-//       />
-//       <button onClick={handlePujar} style={{ padding: '10px' }}>
-//         Pujar
-//       </button>
-
-//       {/* Mostrar el popup si mostrarPopup es verdadero */}
-//       {mostrarPopup && (
-//         <div className="popup">
-//           <p>Puja Realizada</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default BotonPujar;
